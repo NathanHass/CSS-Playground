73a07cc1ee6b35f85d1d1865a1b35785dafb9079
Nathan Hass <nathan@upstatement.com>
2015-05-15 14:29:53 -0400

    minor js tweak

--- static/js/base.js
+++ static/js/base.js
@@ -4,5 +4,7 @@
 
 
 $('.anim-trigger').click(function() {
+  var toggleText = $('.body').hasClass('is-active') ? 'Add Active Class' : 'Remove Active Class';
   $('.body').toggleClass('is-active');
+  $(this).text(toggleText)
 })
